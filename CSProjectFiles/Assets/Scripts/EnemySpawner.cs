da126ead510bb9849c842096cb8ada4d52b568b2
Bhanu <uabhanu@gmail.com>
2016-12-30 12:00:19 +0000

    EnemySpawner logic fixed

--- CSProjectFiles/Assets/Scripts/EnemySpawner.cs
+++ CSProjectFiles/Assets/Scripts/EnemySpawner.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour 
 {
-	private int i;
+    float enemyYPos;
+    int i;
 
 	public GameObject enemyObj;
 	public GameObject[] PF_EnemyCombos;
@@ -17,11 +18,12 @@ public class EnemySpawner : MonoBehaviour
 	{
 		yield return new WaitForSeconds(2.5f);
 		i = Random.Range(0 , PF_EnemyCombos.Length);
-		enemyObj = GameObject.FindGameObjectWithTag("EnemyCombo");
+        enemyYPos = PF_EnemyCombos[i].transform.position.y;
+        enemyObj = GameObject.FindGameObjectWithTag("EnemyCombo");
 
 		if(enemyObj == null) 
 		{
-			enemyObj = Instantiate(PF_EnemyCombos[i], transform.position, transform.rotation) as GameObject;
+			enemyObj = Instantiate(PF_EnemyCombos[i] , new Vector2(transform.position.x , enemyYPos) , transform.rotation) as GameObject;
 		} 
 
 		StartCoroutine("SpawnTimer");
