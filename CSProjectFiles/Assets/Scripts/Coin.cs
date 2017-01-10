3e2c3d0b171e85f0e18c635bcc12b64b824e3d83
Bhanu <uabhanu@gmail.com>
2017-01-10 20:15:59 +0000

    More PickUps Prefabs created Coin Script finished for the time being

--- CSProjectFiles/Assets/Scripts/Coin.cs
+++ CSProjectFiles/Assets/Scripts/Coin.cs
@@ -6,14 +6,53 @@ public class Coin : MonoBehaviour
 {
     AudioSource coinSound;
     CircleCollider2D coinCollider2D;
+    GameObject coinSoundObj;
+    ScoreManager scoreManagementScript;
 
     void Start()
     {
-		
-	}
+        coinCollider2D = GetComponent<CircleCollider2D>();
+        scoreManagementScript = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+        StartCoroutine("SoundObjectTimer");
+    }
 	
 	void Update()
     {
 		
 	}
+
+    IEnumerator SoundObjectTimer()
+    {
+        yield return new WaitForSeconds(0.4f);
+
+        coinSoundObj = GameObject.Find("CoinSound");
+
+        if (coinSoundObj != null)
+        {
+            coinSound = coinSoundObj.GetComponent<AudioSource>();
+        }
+
+        StartCoroutine("SoundObjectTimer");
+    }
+
+    void OnTriggerEnter2D(Collider2D col2D)
+    {
+        if(col2D.gameObject.tag.Equals("Player"))
+        {
+            if (coinSound != null)
+            {
+                if (!coinSound.isPlaying)
+                {
+                    coinSound.Stop();
+                    coinSound.Play();
+                }
+                else
+                {
+                    coinSound.Play();
+                }
+            }
+
+            Destroy(gameObject);
+        }
+    }
 }
