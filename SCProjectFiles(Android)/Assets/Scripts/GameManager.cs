1e48b0ea430993d2b59d52f76483ea71608173fa
uabhanu <uabhanu@gmail.com>
2017-12-17 17:13:20 -0800

    LandRunner UI Finished Chimp Super Mode WIP MainMenu Later some time

--- SCProjectFiles(Android)/Assets/Scripts/GameManager.cs
+++ SCProjectFiles(Android)/Assets/Scripts/GameManager.cs
@@ -7,10 +7,10 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour 
 {
 	AudioSource m_musicSource;
-	Image m_pauseButtonImage , m_pauseMenuImage , m_quitButtonImage , m_restartButtonImage , m_restartAcceptButtonImage , m_restartCancelButtonImage , m_restartMenuImage;
-	Image m_resumeButtonImage;
+	Image m_pauseButtonImage , m_pauseMenuImage , m_quitButtonImage , m_quitAcceptButtonImage , m_quitCancelButtonImage , m_quitMenuImage , m_restartButtonImage , m_restartAcceptButtonImage;
+	Image m_restartCancelButtonImage , m_restartMenuImage , m_resumeButtonImage;
 	string m_currentScene;
-	Text m_restart;
+	Text m_quit , m_restart;
 
 	void Start()
 	{
@@ -18,12 +18,16 @@ public class GameManager : MonoBehaviour
 		m_musicSource = GetComponent<AudioSource>();	
 		m_pauseButtonImage = GameObject.Find("PauseButton").GetComponent<Image>();
 		m_pauseMenuImage = GameObject.Find("PauseMenu").GetComponent<Image>();
+		m_quit = GameObject.Find("QuitText").GetComponent<Text>();
 		m_quitButtonImage = GameObject.Find("QuitButton").GetComponent<Image>();
+		m_quitAcceptButtonImage = GameObject.Find("QuitAcceptButton").GetComponent<Image>();
+		m_quitCancelButtonImage = GameObject.Find("QuitCancelButton").GetComponent<Image>();
+		m_quitMenuImage = GameObject.Find("QuitMenu").GetComponent<Image>();
+		m_restart = GameObject.Find("RestartText").GetComponent<Text>();
 		m_restartButtonImage = GameObject.Find("RestartButton").GetComponent<Image>();
-		m_restartMenuImage = GameObject.Find("RestartMenu").GetComponent<Image>();
 		m_restartAcceptButtonImage = GameObject.Find("RestartAcceptButton").GetComponent<Image>();
 		m_restartCancelButtonImage = GameObject.Find("RestartCancelButton").GetComponent<Image>();
-		m_restart = GameObject.Find("RestartText").GetComponent<Text>();
+		m_restartMenuImage = GameObject.Find("RestartMenu").GetComponent<Image>();
 		m_resumeButtonImage = GameObject.Find("ResumeButton").GetComponent<Image>();
 	}
 
@@ -36,6 +40,37 @@ public class GameManager : MonoBehaviour
 		m_restartButtonImage.enabled = true;
 		m_resumeButtonImage.enabled = true;
 		Time.timeScale = 0;
+	}
+
+	public void Quit()
+	{
+		m_pauseMenuImage.enabled = false;
+		m_quitButtonImage.enabled = false;
+		m_restartButtonImage.enabled = false;
+		m_resumeButtonImage.enabled = false;
+
+		m_quitMenuImage.enabled = true;
+		m_quitAcceptButtonImage.enabled = true;
+		m_quitCancelButtonImage.enabled = true;
+		m_quit.enabled = true;
+	}
+
+	public void QuitAccept()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void QuitCancel()
+	{
+		m_pauseMenuImage.enabled = true;
+		m_quitButtonImage.enabled = true;
+		m_restartButtonImage.enabled = true;
+		m_resumeButtonImage.enabled = true;
+
+		m_quitMenuImage.enabled = false;
+		m_quitAcceptButtonImage.enabled = false;
+		m_quitCancelButtonImage.enabled = false;
+		m_quit.enabled = false;
 	}
 
 	public void Restart()
