eca3bad989d4e506e92c3be09764d95dc8580704
uabhanu <uabhanu@gmail.com>
2020-07-17 22:29:50 +0100

    Killbox Logic now complete and Procedural Generation logic improvement WIP

--- SCProjectFilesAndroid/Assets/Scripts/Misc/Killbox.cs
+++ SCProjectFilesAndroid/Assets/Scripts/Misc/Killbox.cs
@@ -2,40 +2,31 @@
 
 public class Killbox : MonoBehaviour
 {
-    [SerializeField] private Transform _raycastBottom , _raycastTop;
-    //private void Update()
-    //{
-    //    if(Time.timeScale == 0)
-    //    {
-    //        return;
-    //    }
-
-    //    Debug.DrawLine(_raycastTop.position , _raycastBottom.position , Color.red);
-    //    RaycastHit2D hit2D = Physics2D.Raycast(_raycastTop.position , _raycastBottom.position);
-
-    //    if(hit2D)
-    //    {
-    //        Debug.Log("Killbox : " + hit2D.collider.gameObject);
-    //        Kill(hit2D.collider.gameObject);
-    //    }
-    //}
-
     void Kill(GameObject gameObject)
     {
         Destroy(gameObject);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision2D)
-    {
-        Debug.Log("Killbox : " + collision2D.gameObject);
-    }
-
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
+        if(collider2D.gameObject.tag.Equals("Clouds"))
+        {
+            Kill(collider2D.gameObject);
+        }
+
         if(collider2D.gameObject.tag.Equals("Collectibles"))
         {
             Kill(collider2D.gameObject);
-            Debug.Log("Killbox : " + collider2D.gameObject);
+        }
+
+        if(collider2D.gameObject.tag.Equals("Mountains"))
+        {
+            Kill(collider2D.gameObject);
+        }
+
+        if(collider2D.gameObject.tag.Equals("Platform"))
+        {
+            Kill(collider2D.gameObject);
         }
     }
 }
