49565d51d74bff0c9ec40f2331cfaa0a208f9441
uabhanu <uabhanu@gmail.com>
2020-07-29 19:12:40 +0100

    1. Updated PlatformGeneration logic by removing random y positions for the platforms as that's not required 2. Parallax Bug Fix using LastEndPosition Offset through CloudsGeneratior WIP 3. Parallax Bug Fix using the Clouds Generator Logic for FarTrees & Trees Generator Scripts Up Next

--- SCProjectFilesAndroid/Assets/Scripts/Misc/CloudsGenerator.cs
+++ SCProjectFilesAndroid/Assets/Scripts/Misc/CloudsGenerator.cs
@@ -4,6 +4,7 @@ public class CloudsGenerator : MonoBehaviour
 {
     //TODO Backgrounds are overlapping due to the parallax and the reason is, this logic uses EndPosition of the 1st Part but when these are moving, it's no longer enough
     private const float PLAYER_DISTANCE_SPAWN_CLOUDS_PART = 40.0f;
+    [SerializeField] private float _endPositionOffset;
 
     [SerializeField] private Transform _cloudsEndPosition;
     [SerializeField] private Transform _cloudsPartToSpawn;
@@ -13,6 +14,8 @@ public class CloudsGenerator : MonoBehaviour
     private void Awake() 
     {
         _lastEndPosition = _cloudsEndPosition.transform.position;
+        _endPositionOffset = _lastEndPosition.x - _landPuss.GetPosition().x;
+        Debug.Log("Last End Position Position Offset : " + _endPositionOffset);
     }
 
     private void Update() 
@@ -20,6 +23,7 @@ public class CloudsGenerator : MonoBehaviour
         if(Vector3.Distance(_landPuss.GetPosition() , _lastEndPosition) < PLAYER_DISTANCE_SPAWN_CLOUDS_PART) 
         {
             // Spawn another clouds part
+            //Debug.Log("Distance between player & Clouds : " + Vector3.Distance(_landPuss.GetPosition() , _lastEndPosition));
             SpawnCloudsPart();
         }
     }
@@ -29,11 +33,14 @@ public class CloudsGenerator : MonoBehaviour
         Transform chosenCloudsPart = _cloudsPartToSpawn;
         Transform lastCloudsPartTransform = SpawnCloudsPart(chosenCloudsPart , _lastEndPosition);
         _lastEndPosition = lastCloudsPartTransform.Find("EndPosition").position;
+        _endPositionOffset = _lastEndPosition.x - _landPuss.GetPosition().x;
+        Debug.Log("Last End Position Offset : " + _endPositionOffset);
+        //Debug.Log("Last End position : " + _lastEndPosition);
     }
 
     private Transform SpawnCloudsPart(Transform cloudsPart , Vector3 spawnPosition) 
     {
-        Transform cloudsPartTransform = Instantiate(cloudsPart , new Vector3(spawnPosition.x , 0.33f , spawnPosition.z) , Quaternion.identity);
+        Transform cloudsPartTransform = Instantiate(cloudsPart , new Vector3(spawnPosition.x  , 0.33f , spawnPosition.z) , Quaternion.identity);
         return cloudsPartTransform;
     }
 }
