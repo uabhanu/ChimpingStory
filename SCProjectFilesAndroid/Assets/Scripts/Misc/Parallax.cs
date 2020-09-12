96453b73dbfcac195d41313c821f45522259eb15
uabhanu <uabhanu@gmail.com>
2020-09-12 13:07:17 +0100

    1. LandParallax & WaterParallax now combined to one Parallax script and applied that to Space Level as well 2. APK built successfully

---
+++ SCProjectFilesAndroid/Assets/Scripts/Misc/Parallax.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float _parallaxOffset;
+    [SerializeField] private string _pussTypeToLookFor;
+
+    [SerializeField] private LandPuss _landPussReference;
+    [SerializeField] private SuperPuss _superPussReference;
+    [SerializeField] private WaterPuss _waterPussReference;
+
+    private void Update()
+    {
+        if(Time.timeScale == 0)
+        {
+            return;
+        }
+
+        switch(_pussTypeToLookFor)
+        {
+            case "Land Puss":
+                transform.Translate(Vector2.right * _landPussReference.GetMoveSpeed() * _parallaxOffset * Time.deltaTime , Space.World);
+            break;
+
+            case "Super Puss":
+                transform.Translate(Vector2.right * _superPussReference.GetMoveSpeed() * _parallaxOffset * Time.deltaTime , Space.World);
+            break;
+
+            case "Water Puss":
+                transform.Translate(Vector2.left * _waterPussReference.GetMoveSpeed() * _parallaxOffset * Time.deltaTime , Space.World);
+            break;
+        }
+    }
+}
